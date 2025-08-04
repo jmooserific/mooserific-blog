cdf6dc7b10784d545cd11e4a9516cfd371aa4382
John Moose <jmooserific@users.noreply.github.com>
2025-08-04 19:59:27 -0400

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,12 @@ import { inter, sacramento } from './fonts'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Mooserific Blog!</title>
+        <meta name="description" content="A family photo blog!" />
+      </head>
       <body className={`${inter.className} bg-gray-50 text-gray-900 antialiased min-h-screen`}>
         <header>
           <h1 className={`${sacramento.className} text-6xl font-bold m-6 text-gray-900 text-center`}>Mooserific!</h1>
@@ -11,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main>
           {children}
         </main>
-        <footer className="text-center text-sm text-gray-400 py-6">
+        <footer className="text-center text-sm text-gray-900 py-6">
           &copy; {new Date().getFullYear()} Mooserific
         </footer>
       </body>
