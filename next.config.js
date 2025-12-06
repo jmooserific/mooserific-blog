c89b87b06f53990da5c2859b4145a6abbe867b74
John Moose <jmooserific@users.noreply.github.com>
2025-12-06 11:10:17 -0500

    Add missing image quality

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     deviceSizes: [320, 480, 768, 1024],
     imageSizes: [120, 160, 240, 320],
+    qualities: [60, 75],
     unoptimized: false,
     // Allow Cloudflare R2 public bucket domain (set R2_PUBLIC_HOSTNAME)
     remotePatterns: [
